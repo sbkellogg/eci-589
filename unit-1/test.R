d409c433a725e39c1f4330f0c410eec93fd4f537
Shaun Kellogg <sbkellog@ncsu.edu>
2021-06-03 16:41:55 -0400

    unit 1 draft complete

--- unit-1/test.R
+++ unit-1/test.R
@@ -166,3 +166,13 @@ ggraph(net, layout="fr") +
   geom_node_point(aes(color = Facilitator,
                       size = deg*.5)) +  # size by audience size  
   theme_void()
+
+layouts <- grep("^layout_", ls("package:igraph"), value=TRUE)[-1] 
+# Remove layouts that do not apply to our graph.
+layouts <- layouts[!grepl("bipartite|merge|norm|sugiyama|tree", layouts)]
+
+par(mfrow=c(3,3), mar=c(1,1,1,1))
+for (layout in layouts) {
+  print(layout)
+  l <- do.call(layout, list(weighted_network)) 
+  plot(weighted_network, edge.arrow.mode=0, layout=l, main=layout) }
