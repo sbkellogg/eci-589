aecb172822bd0976d7f21616d9662e36c6f8488e
Shaun Kellogg <sbkellog@ncsu.edu>
2021-07-19 13:00:32 +0000

    Wrangling done

--- test.R
+++ test.R
@@ -2,6 +2,7 @@
 library(RCurl)
 library(tidygraph)
 library(igraph)
+library(readxl)
 
 ga.mat<-getURL("https://docs.google.com/spreadsheet/pub?key=0Ai--oOZQWBHSdDE3Ynp2cThMamg1b0VhbEs0al9zV0E&single=true&gid=0&output=txt",
                ssl.verifypeer = FALSE)
@@ -16,33 +17,87 @@ ga.atts<-read.table(textConnection(ga.atts), sep="\t", header=T, quote="\"",
 ga.net<-network(ga.mat, vertex.attr=ga.atts, vertex.attrnames=colnames(ga.atts),
                 directed=F, hyper=F, loops=F, multiple=F, bipartite=F)
 
+# WRANGLE
+
+
 # Import Data
 
-matrix <- read_csv("unit-3/data/School Leaders Data Chapter 9_d.csv") %>%
-  as.matrix()
-
-class(matrix)
-
-
-
-adjacency_matrix <- graph.adjacency(matrix,
-                             weighted = TRUE)
-
-class(adjacency_matrix)
-
-edge_list <- get.data.frame(adjacency_matrix)
-
-edge_list
+edge_matrix <- read_csv("unit-3/data/School Leaders Data Chapter 9_d.csv")
 
 
 node_list <- 
   read_excel("unit-3/data/School Leaders Data Chapter 9_e.xlsx", 
              col_types = c("text", "numeric", "numeric", "numeric", "numeric"))
 
-network <- tbl_graph(edges = edge_list,
-                     nodes = node_list)
 
-network
+## Convert to Matrix
+
+matrix <- edge_matrix %>%
+  as.matrix()
+
+class(matrix)
+
+## Dichotomize matrix
+
+matrix[matrix <= 2] <- 0
+
+matrix[matrix >= 3] <- 1
+
+matrix
+
+## Convert Adjacent Matrix 
+
+adjacency_matrix <- graph.adjacency(matrix)
+
+class(adjacency_matrix)
+
+
+## Convert to edge list
+
+
+
+edge_list <- get.data.frame(adjacency_matrix) %>%
+  to_simple()
+
+
+edge_list
+
+## Create Network Object 
+
+network <- tbl_graph(edges = edge_list,
+                     nodes = node_list,
+                     directed = TRUE)
+
+simple_network <- network %>%
+  convert(to_simple)
+
+  
+
+simple_network
+
+
+
+plot(simple_network) 
+
+
+# EXPLORE
+
+# Degree
+
+network_measures <- simple_network %>%
+  activate(nodes) %>%
+  mutate(degree = centrality_degree(mode = "all")) %>%
+  mutate(in_degree = centrality_degree(mode = "in")) %>%
+  mutate(out_degree = centrality_degree(mode = "out"))
+
+network_measures
+
+node_measures <- network_measures %>% 
+  activate(nodes) %>%
+  data.frame()
+
+summary(node_measures)
+
 
 ergm_network <- network(edge_list, vertex.attr = node_list, matrix.type = "edgelist", ignore.eval = FALSE, loops = TRUE)
 
