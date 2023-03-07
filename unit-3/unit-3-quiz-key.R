4eca5b420d120f2fd56a24b2af349c1189b84f4f
sbkellogg <sbkellog@ncsu.edu>
2023-03-07 10:17:27 -0500

    quiz complete

--- unit-3/unit-3-quiz-key.R
+++ unit-3/unit-3-quiz-key.R
@@ -1,3 +1,27 @@
+library(tidyverse)
+library(tidytext)
+library(tidygraph)
+library(ggraph)
+
+ccss_tweets <- read_csv("unit-3/data/ccss-tweets-simple.csv")
+
+ccss_edges <- ccss_tweets |>
+  select(user_id,
+         status_id,
+         created_at,
+         screen_name,
+         reply_to_screen_name,
+         quoted_screen_name, 
+         hashtags)
+
+write_csv(ccss_edges, "unit-3/data/ccss-edges.csv")
+
+ccss_text <- ccss_tweets |>
+  select(status_id,
+         text)
+
+write_csv(ccss_text, "unit-3/data/ccss-text.csv")
+
 #### Question 1 Import Data ####
 
 # Load the appropriate package(s) and use the read_csv( ) function to import the "ccss-edges.csv" file located in your data folder. Save to your R environment as an object named "ccss_edges". 
@@ -6,9 +30,11 @@
 #
 # Use a function or method of your choosing to inspect your data and then match the following variables with the correct definitions. 
 
+ccss_edges <- read_csv("unit-3/data/ccss-edges.csv")
 
+glimpse(ccss_edges)
 
-
+ccss_edges
 
 #### Question 2 Count Distinct Values #### 
 
@@ -16,9 +42,14 @@
 #   
 # Hint:  Actors are identified by their Twitter "screen_name" and the tidyverse package has a function for selecting the unique/distinct rows from a data frame. 
 
+distinct(ccss_edges, screen_name) |>
+  count()
 
+distinct(ccss_edges, status_id) |>
+  count()
 
-
+ccss_edges |>
+count(screen_name, sort = T)
 
 
 #### Question 3 Split Column ####
@@ -29,6 +60,17 @@
 # 
 # On which day were the most tweets posted? 
 
+ccss_separate <- ccss_edges |>
+  separate(created_at, 
+           into = c("date", 
+                    "time"), 
+           sep = " ")
+
+
+distinct(ccss_separate, date) |>
+  arrange()
+
+count(ccss_separate, date)
 
 
 
@@ -42,12 +84,11 @@
 #   
 #   Hint: Look up the unite() function in the Help tab to figure out how might you deal with missing values, i.e., NA. Also, be mindful of the rows combining two missing values when taking your count.
 
-
-
-
-
-
-
+ccss_unite <- ccss_separate |>
+  unite(to_screen_name, 
+        reply_to_screen_name, 
+        quoted_screen_name,
+        na.rm = TRUE)
 
 # ccss_united <- ccss_separate |>
 #   unite(target, 
@@ -55,20 +96,22 @@
 #         quoted_screen_name)
 
 
+distinct(ccss_unite, to_screen_name) |>
+  count()
 
-
-
+count(ccss_unite, to_screen_name)
 
 ### Question 5 Pivot Columns ####
 
 # use the read_csv( ) function to import the "ccss-edges.csv" file located in your data folder. Save to your R environment as an object named "ccss_edges". 
 
+ccss_text <- read_csv("unit-3/data/ccss-text.csv")
 
+ccss_join <- left_join(ccss_unite,
+                       ccss_text,
+                       by = "status_id")
 
-
-
-
-
+ccss_join
 
 
 ### Question 6 Joing Data Frames ####
@@ -77,13 +120,15 @@
 # 
 # How many total unique actors are in our network? 
 #   
-# Hint: Ignore the blank row(s) that resulted from combining the empty reply_to_screen_name and quoted_screen_name columns. 
+#   Hint: Ignore the blank row(s) that resulted from combining the empty reply_to_screen_name and quoted_screen_name columns. 
 
+ccss_actors <- ccss_unite |> 
+  gather(key = "direction", 
+         value = "name",
+         screen_name,
+         to_screen_name) |>
+  filter(name != "")
 
+ccss_actors
 
-
-
-
-
-
-
+ccss_actors |> count(name)
