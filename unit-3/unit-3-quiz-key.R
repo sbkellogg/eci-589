fa8876bdc4bf7922af2f8e0d77cb5306a67d226f
sbkellogg <sbkellog@ncsu.edu>
2023-03-13 10:44:46 -0500

    icloud cruft and case study fix

--- unit-3/unit-3-quiz-key.R
+++ unit-3/unit-3-quiz-key.R
@@ -48,7 +48,7 @@ distinct(ccss_edges, screen_name) |>
 distinct(ccss_edges, status_id) |>
   count()
 
-ccss_tweets |>
+ccss_edges |>
 count(screen_name, sort = T)
 
 
@@ -126,10 +126,9 @@ ccss_actors <- ccss_unite |>
   gather(key = "direction", 
          value = "name",
          screen_name,
-         to_screen_name)
-
-
-ccss_actors |> count(name)
+         to_screen_name) |>
+  filter(name != "")
 
 ccss_actors
 
+ccss_actors |> count(name)
